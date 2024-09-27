da29f4cffb645fb665306184ec8007572619a4c5
windfollowingheart <2916311184@qq.com>
2024-09-27 21:00:33 +0800

    1

---
+++ test_gpu.py
@@ -0,0 +1,7 @@
+import pynvml
+import tensorflow as tf
+pynvml.nvmlInit()
+gpu_num = pynvml.nvmlDeviceGetCount()
+# check nvidia driver
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(f"[INFO]: gpu_num : {gpu_num}, gpus: {len(gpus)}")
