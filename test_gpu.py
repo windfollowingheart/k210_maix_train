e997cba9bee23280c56e8594749fcaa0518a9eb6
windfollowingheart <2916311184@qq.com>
2024-09-27 21:01:58 +0800

    1

--- test_gpu.py
+++ test_gpu.py
@@ -5,3 +5,4 @@ gpu_num = pynvml.nvmlDeviceGetCount()
 # check nvidia driver
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print(f"[INFO]: gpu_num : {gpu_num}, gpus: {len(gpus)}")
+print(tf.__version__)
