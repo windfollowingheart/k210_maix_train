5a385972a5519bec46f803b5eaffd7f625b00588
windfollowingheart <2916311184@qq.com>
2024-09-29 18:35:10 +0800

    1

--- test_gpu.py
+++ test_gpu.py
@@ -1,8 +1,9 @@
 import pynvml
 import tensorflow as tf
+
 pynvml.nvmlInit()
 gpu_num = pynvml.nvmlDeviceGetCount()
 # check nvidia driver
-gpus = tf.config.experimental.list_physical_devices('GPU')
+gpus = tf.config.experimental.list_physical_devices('CPU')
 print(f"[INFO]: gpu_num : {gpu_num}, gpus: {len(gpus)}")
 print(tf.__version__)
